7d443fa6991093d43706a42938864c54dd05a49b
worthworld <1135257206@qq.com>
2021-07-01 22:59:25 +0800

    ed:部署gthub pages

---
+++ .vuepress/config.js
@@ -0,0 +1,103 @@
+module.exports = {
+  "title": "左右同学的个人空间",
+  "description": "追求理想与现实的平衡，左右为难",
+  "dest": "docs",
+  "base":"/vue-press-blog/",
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "/favicon.ico"
+      }
+    ],
+    [
+      "meta",
+      {
+        "name": "viewport",
+        "content": "width=device-width,initial-scale=1,user-scalable=no"
+      }
+    ]
+  ],
+  "theme": "reco",
+  "themeConfig": {
+    "nav": [
+      {
+        "text": "主页",
+        "link": "/",
+        "icon": "reco-home"
+      },
+      {
+        "text": "时间抽",
+        "link": "/timeline/",
+        "icon": "reco-date"
+      },
+      {
+        "text": "文档",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "vuepress-reco",
+            "link": "/document/theme-reco/"
+          }
+        ]
+      },
+      {
+        "text": "关于",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "GitHub",
+            "link": "https://github.com/worthworld",
+            "icon": "reco-github"
+          }
+        ]
+      }
+    ],
+    "sidebar": {
+      "/document/theme-reco/": [
+        "",
+        "theme",
+        "plugin",
+        "api"
+      ]
+    },
+    "type": "blog",
+    "blogConfig": {
+      "title":"分类",
+      "category": {
+        "location": 2,
+        "text": "分类"
+      },
+      "tag": {
+        "location": 3,
+        "text": "标签"
+      }
+    },
+    "friendLink": [
+      {
+        "title": "午后南杂",
+        "desc": "Enjoy when you can, and endure when you must.",
+        "email": "1156743527@qq.com",
+        "link": "https://www.recoluan.com"
+      },
+      {
+        "title": "淡若清风",
+        "desc": "不是每个人都应该像我这样去建造一座水晶大教堂，但是每个人都应该拥有自己的梦想，设计自己的梦想，追求自己的梦想，实现自己的梦想。",
+        "avatar": "/along.png",
+        "link": "http://www.betteralong.top/#/"
+      }
+    ],
+    "logo": "/logo.png",
+    "search": true,
+    "searchMaxSuggestions": 10,
+    "lastUpdated": "Last Updated",
+    "author": "左右",
+    "authorAvatar": "/avatar.jpg",
+    "record": "xxxx",
+    "startYear": "2021"
+  },
+  "markdown": {
+    "lineNumbers": true
+  }
+}
